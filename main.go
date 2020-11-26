bb9f8e0ddf3e850cbdda1e1f463d14b5806ef3eb
Vincent Marguerie <v.marguerie@quortex.io>
2020-11-26 09:06:33 +0100

    bootstrap scheduler api

--- main.go
+++ main.go
@@ -82,6 +82,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Instance")
 		os.Exit(1)
 	}
+	if err = (&controllers.SchedulerReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Scheduler"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Scheduler")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
